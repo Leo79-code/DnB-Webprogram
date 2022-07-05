773ecdb22be09bc6b7d72ea702124a56aa9538c6
Leo79 <791829242@qq.com>
2022-07-05 18:17:19 +0800

    axois封装以及跨域问题的解决

--- dnb-vuejs/src/main.js
+++ dnb-vuejs/src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import request from "@/utils/request";
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(ElementUI, {size: "small"});
 Vue.config.productionTip = false
+Vue.prototype.request=request
 
 new Vue({
   router,
