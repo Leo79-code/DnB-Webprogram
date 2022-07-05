28d95a30b7d8566c23ba9a11fd3b44d0bbf5d7c0
Leo79 <791829242@qq.com>
2022-07-05 10:36:12 +0800

    完成了Login界面以及相关router

--- dnb-vuejs/src/views/Manage.vue
+++ dnb-vuejs/src/views/Manage.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <el-container style="height: 100%;">
+  <div>
+    <el-container style="height: 100%; border: 1px solid #eee">
       <el-aside width="200px" style="background-color: rgb(238, 241, 246); height: 100%">
         <el-menu :default-openeds="['1', '3']">
           <el-submenu index="1">
@@ -76,9 +76,20 @@
         </el-main>
       </el-container>
     </el-container>
-
   </div>
 </template>
+
+<style>
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
 
 <script>
 export default {
