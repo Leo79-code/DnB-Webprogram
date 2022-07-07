e1e971acf54cd97f5429821014e006b2f0f13331
PirxTion <1550748608@qq.com>
2022-07-08 05:47:02 +0800

    Update ManagerMapper.java

--- administrator-side-software/src/main/java/com/example/administratorsidesoftware/mapper/ManagerMapper.java
+++ administrator-side-software/src/main/java/com/example/administratorsidesoftware/mapper/ManagerMapper.java
@@ -2,7 +2,6 @@ package com.example.administratorsidesoftware.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.administratorsidesoftware.entity.Manager;
-import com.example.administratorsidesoftware.entity.Position;
 
 public interface ManagerMapper extends BaseMapper<Manager> {
     Manager getPositionManager(Integer positionNo);
