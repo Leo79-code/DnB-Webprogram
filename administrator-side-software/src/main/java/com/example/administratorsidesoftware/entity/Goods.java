fd2f982a14f91d4addbc6084fcaf17af87e16834
PirxTion <1550748608@qq.com>
2022-07-08 22:21:40 +0800

    goods

--- administrator-side-software/src/main/java/com/example/administratorsidesoftware/entity/Goods.java
+++ administrator-side-software/src/main/java/com/example/administratorsidesoftware/entity/Goods.java
@@ -4,13 +4,8 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
 import com.example.administratorsidesoftware.common.GoodsType;
-import com.example.administratorsidesoftware.mapper.GoodsMapper;
-import com.example.administratorsidesoftware.service.GoodsService;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 
