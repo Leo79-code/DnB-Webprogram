21d78bdbd0934059460d65edcca92e97e8fcef26
xisW <unicorn_girl@163.com>
2022-07-08 21:54:21 +0800

    Revert "验证码前端部分"
    This reverts commit d7ec0d89617925ca4961f2192b1e61e9f16d1e2f.

--- dnb-vuejs/src/utils/request.js
+++ dnb-vuejs/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const request = axios.create({
-    baseURL: 'http://127.0.0.1:8080', // hCaptcha CORS 要求
+    baseURL: 'http://localhost:8080',
     timeout: 5000
 })
 
